b60f2b4a91499e179e27dbd50bbda04a78f235f0
Raphael Planer <58560217+DevRaeph@users.noreply.github.com>
2022-02-28 15:23:01 +0100

    Create TestCase.php

---
+++ tests/TestCase.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace DevRaeph\PDFPasswordProtect\Tests;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Orchestra\Testbench\TestCase as Orchestra;
+use DevRaeph\PDFPasswordProtect\PDFPasswordProtectServiceProvider ;
+
+class TestCase extends Orchestra
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+
+    }
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            PdfSignerServiceProvider::class,
+        ];
+    }
+
+    public function getEnvironmentSetUp($app)
+    {
+        config()->set('database.default', 'testing');
+
+        /*
+        $migration = include __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
+        $migration->up();
+        */
+    }
+}
